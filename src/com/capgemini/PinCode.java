816f775509c4c45f3b769534eb42c35b9baf3e8f
krove <krove@DESKTOP-Q5SED1R>
2020-09-29 03:57:10 +0530

    [Kashif]UC1 added. Pin length

--- src/com/capgemini/PinCode.java
+++ src/com/capgemini/PinCode.java
@@ -1,7 +1,23 @@
 package com.capgemini;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class PinCode {
-public static void main(String[] args) {
-	System.out.println("Welcome.");
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Pattern p = Pattern.compile("[0-9]{6}");
+		while (true) {
+			System.out.println("Enter the PIN Code: ");
+			String pin = sc.nextLine();
+			Matcher m = p.matcher(pin);
+			if (m.find()) {
+				System.out.println("Valid PIN Code !");
+				break;
+			} else {
+				System.out.println("ERROR ! Not a valid PIN Code. Please re-eneter:-\n");
+			}
+		}sc.close();
+	}
+
 }
